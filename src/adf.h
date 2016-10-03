e7bd5757cd43782635be49131aabc34f283fcaba
Steve Fosdick <other@fosdick.me.uk>
2016-10-03 21:37:19 +0100

    Revert a little of the previous change

--- src/adf.h
+++ src/adf.h
@@ -1,3 +1,5 @@
 void adf_init();
 void adf_load(int drive, char *fn);
+void adl_load(int drive, char *fn);
+void adl_loadex(int drive, char *fn, int sectors, int size, int dblstep);
 void adf_close(int drive);
